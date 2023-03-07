4763a262da29b317fdf8d7c6afa396c2fc762d34
Carl <carlst99@outlook.co.nz>
2023-03-08 03:16:23 +1300

    Nullable Zone.cs#Name,Description

--- squittal.ScrimPlanetmans.App/Models/Planetside/Zone.cs
+++ squittal.ScrimPlanetmans.App/Models/Planetside/Zone.cs
@@ -7,8 +7,8 @@ public class Zone
     [Required]
     public int Id { get; set; }
 
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
     public string Code { get; set; }
     public int? HexSize { get; set; }
 }
